46f0aba2d7536256e86d54f71baf8f3338ac9df7
Kartar <kartar@pushapp.co.uk>
2024-12-03 19:59:41 +0000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kartar Singh Jabanda on 03/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
